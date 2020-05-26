0b3100e0d3148b03651e51790fc471d1cc8d323f
king <tettehmel@gmail.com>
2020-05-26 09:26:30 -0400

    test

---
+++ 00-greetFriend/greetFriend.js
@@ -0,0 +1,11 @@
+function greetFriend(name) {
+  // Insert code here;
+  var str = "Hello, " +name;
+  
+  return str;
+}
+
+let result = greetFriend("Albert");
+console.log("result",result);
+// Do not edit this line;
+module.exports = greetFriend;
