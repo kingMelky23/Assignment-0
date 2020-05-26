c553a428ba12e14d97fc3a03ffa5b0e05521b2cd
king <tettehmel@gmail.com>
2020-05-26 12:48:10 -0400

    passed 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,8 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  return title.split(' ').map(function(word){
+    return word[0].toUpperCase()+word.slice(1).toLowerCase();
+  }).join(' ');
 }
 
 // Do not edit this line;
