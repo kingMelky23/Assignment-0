64607303dc288b767e43e6538d4de99f2259d824
king <tettehmel@gmail.com>
2020-05-26 21:29:38 -0400

    04 passed

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -2,13 +2,10 @@ function countOfAllBooleans(arr) {
   // Insert code here;
   let count = 0;
 for(let i=0; i<arr.length; i++){
-if(typeof arr[i] ===  "boolean"){
-count++;}
-}
-return count;
-
-
-    
+  if(typeof arr[i] ===  "boolean"){
+  count++;}
+  }
+  return count;
 }
 
 
