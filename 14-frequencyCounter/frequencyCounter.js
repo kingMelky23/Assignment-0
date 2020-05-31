915a36158d04324f1184dd0902a26885df04c7c6
king <tettehmel@gmail.com>
2020-05-31 02:20:19 -0400

    complete 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,23 @@
 function frequencyCounter(word) {
   // Insert code here;
+  //create empty object
+  let freq={}
+
+  //loop through all the char in the string
+  for(let i = 0; i <word.length; i++){
+    //check if letter is in the object
+    if(freq[word[i]]){
+      //add 1 to counter
+      freq[word[i]]+=1
+    }
+    else{
+      //else create a new counter for the new element
+      freq[word[i]] =1
+    }
+  }
+  return freq
 }
+
 
 // Do not edit this line;
 module.exports = frequencyCounter;
